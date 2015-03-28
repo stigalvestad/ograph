e2c408ca474f6b84afd95a18d6a7eda0c12d31d9
Stig A <stigalvestad@gmail.com>
2015-03-28 17:27:02 +0100

    Removed file writing to local folder

--- app/services/CommonTasks.scala
+++ app/services/CommonTasks.scala
@@ -14,7 +14,7 @@ class CommonTasks {
   def fetchedAndProcessedOk(log: RetrievalLog): Boolean = log.statusCode == 200 && log.processedOk == Some(true)
   
   def logEventorRequest(r: WSResponse, log: RetrievalLog, logDAO: LogDAO): Future[RetrievalLog] = {
-    Files.writeFile(new File(s"/home/stig/code/ograph_project/downloaded_xml/${log.resourceType}-${log.eventorId}.xml"), r.body)
+//    Files.writeFile(new File(s"/home/stig/code/ograph_project/downloaded_xml/${log.resourceType}-${log.eventorId}.xml"), r.body)
     logDAO.insert(log).map { i => log }
   }
   
